af632637e49194f7eb10deec7579ea971ca10ebd
marcocentonze <marco.centonze27@gmail.com>
2023-10-23 19:09:42 +0200

    fixed

--- Database/db.php
+++ Database/db.php
@@ -13,18 +13,19 @@ try {
 array_push(
     $pet_products,
 
-    $dogFood = new Food('Dog Food', 0, $dog_category, 'High-quality dog food for your furry friend', '2kg'),
+    $dogFood = new Food('Dog Food', 10.99, $dog_category, 'High-quality dog food for your furry friend', '2kg'),
     $catFood = new Food('Cat Food', 18.99, $cat_category, 'Delicious cat food for a happy feline', '1.5kg'),
     $squeakyBall = new Toy('Squeaky Ball', 9.99, $dog_category, 'Interactive squeaky ball for hours of fun', 'Small'),
     $featherWand = new Toy('Feather Wand', 6.99, $cat_category, 'Interactive feather wand toy for playful cats', 'Small'),
     $dogKennel = new Kennel('Dog Kennel', 89.99, $dog_category, 'Large', 'Durable plastic'),
     $catKennel = new Kennel('Cat Kennel', 49.99, $cat_category, 'Small', 'Soft fabric')
 );
+//setcolor 
+$dogKennel->setColor('Black');
+$catKennel->setColor('Red');
+
 } catch (Exception $e) {
     echo $e->getMessage();
 }
 
 
-//setcolor 
-$dogKennel->setColor('Black');
-$catKennel->setColor('Red');
--- Models/Product.php
+++ Models/Product.php
@@ -10,7 +10,7 @@ class Product
     function __construct($_name, $_price, $_category)
     {
         $this->name = $_name;
-        $this->price = $_price;
+        $this->setPrice($_price);
         $this->category = $_category;
     }
 
